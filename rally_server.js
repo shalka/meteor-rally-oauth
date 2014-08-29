08c66e8acb04097e74927c3b9a10f46135b6bf16
Stanley Halka <shalka@rallydev.com>
2014-08-28 22:24:35 -0400

    make additional call to /userprofile to load rally default workspace and project on login

--- rally_server.js
+++ rally_server.js
@@ -2,11 +2,13 @@ var Oauth = Package.oauth.Oauth;
 var urlUtil = Npm.require('url');
 
 Oauth.registerService('rally', 2, null, function(query) {
-
   var response = getAccessToken(query);
   var accessToken = response.accessToken;
   var expiresAt = response.expiresAt;
   var identity = getIdentity(accessToken);
+  var user = identity.User;
+  var profile = getUserProfile(accessToken, user.UserProfile._ref);
+  var userprofile = profile.UserProfile;
 
   // fields to pluck from the 'User' map of the retrieved identity
   //  each key is the field as it exists on the 'User' map
@@ -24,29 +26,68 @@ Oauth.registerService('rally', 2, null, function(query) {
     '_refObjectUUID': 'object_uuid'
   };
 
-  var user = identity.User;
   // use ObjectID as 'id' field in service data
+  // => consider switching to _refObjectUUID
+  // userprofile will contain the following fields:
+  // "DefaultProject": {
+  //   "_ref": "https://rally1.rallydev.com/slm/webservice/v2.x/project/<oid>",
+  //   "_refObjectUUID": "<uuid>",
+  //   "_refObjectName": "<name>",
+  //   "_type": "Project"
+  // },
+  // "DefaultWorkspace": {
+  //   "_ref": "https://rally1.rallydev.com/slm/webservice/v2.x/workspace/<oid>",
+  //   "_refObjectUUID": "<uuid>",
+  //   "_refObjectName": "<name>",
+  //   "_type": "Workspace"
+  // },
+  var defaultworkspace = {};
+  var defaultproject = {};
+  if (userprofile.DefaultWorkspace) {
+    defaultworkspace = {
+      name: userprofile.DefaultWorkspace._refObjectName,
+      oid: userprofile.DefaultWorkspace._ref.split('/').pop(),
+      uuid: userprofile.DefaultWorkspace._refObjectUUID
+    };
+  }
+  if (userprofile.DefaultProject) {
+    defaultproject = {
+      name: userprofile.DefaultProject._refObjectName,
+      oid: userprofile.DefaultProject._ref.split('/').pop(),
+      uuid: userprofile.DefaultProject._refObjectUUID
+    };
+  }
   var data = {
     id: user.ObjectID,
     accessToken: accessToken,
-    expiresAt: expiresAt
+    expiresAt: expiresAt,
+    defaults: {
+      workspace: defaultworkspace,
+      project: defaultproject
+    }
   };
 
   _.each(identityFields, function(transformedField, identityField) { data[transformedField] = user[identityField]; });
 
-  // format of image url will always be: https://rally1.rallydev.com/slm/profile/image/#{ObjectID}/80.sp
+  // format of image url will always be: https://rally1.rallydev.com/slm/profile/image/#{User.ObjectID}/80.sp
   data.picture = 'https://rally1.rallydev.com/slm/profile/image/' + user.ObjectID + '/80.sp';
   data.locale = 'en';
-  // data.timezone = 'EDT'; (out of scope for now)
-  // => timezone lookup from office location
-  //  OR
-  // => query https://rally1.rallydev.com/slm/webservice/v2.x/userprofile/#{ObjectID} and pluck TimeZone field
+  data.timezone = userprofile.TimeZone;
 
-  var fields = { name: data.first_name + ' ' + data.last_name };
+  name = data.first_name;
+  if (data.last_name != null) {
+    name = name + ' ' + data.last_name
+  }
+
+  var options = {
+    profile: {
+      name: name
+    }
+  };
 
   return {
     serviceData: data,
-    options: { profile: fields }
+    options: options
   };
 });
 
@@ -84,7 +125,7 @@ var getAccessToken = function (query) {
   }
 
   // parsed response will resemble:
-  // { 
+  // {
   //   token_type: 'Bearer',
   //   expires_in: <number of seconds>,
   //   access_token: '<access token value>',
@@ -113,10 +154,19 @@ var getExpiresAt = function (expiresIn) {
 var getIdentity = function (accessToken) {
   try {
     // need to pass zsessionid header (acts as bearer token) to retrieve user information
-    return Meteor.http.get("https://rally1.rallydev.com/slm/webservice/v2.x/user", 
+    return Meteor.http.get("https://rally1.rallydev.com/slm/webservice/v2.x/user",
       {headers: {zsessionid: accessToken, format: 'json'}}).data;
   } catch (err) {
     throw new Error("Failed to fetch identity from Rally. " + err.message);
+  }
+};
+
+var getUserProfile = function(accessToken, rallyUserProfileUrl) {
+  try {
+    // need to pass zsessionid header (acts as bearer token) to retrieve user information
+    return Meteor.http.get(rallyUserProfileUrl, {headers: {zsessionid: accessToken, format: 'json'}}).data;
+  } catch (err) {
+    throw new Error("Failed to fetch user profile from Rally. " + err.message);
   }
 };
 
